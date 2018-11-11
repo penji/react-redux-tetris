5530923fc6dc3d88595618aff994ffe604efb6e8
penji <11308346+penji@users.noreply.github.com>
2018-11-12 05:39:49 +0900

    store에 redux-thunk 미들웨어 추가 적용

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,20 @@
-import { createStore } from 'redux';
-import reducer from '../reducer/index';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from '../reducer/index';
 
-const store = createStore(
-    reducer,
-    /* preloadedState, */
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+
+const composeEnhancers =
+    typeof window === 'object' &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+          // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+        }) : compose;
+
+const enhancer = composeEnhancers(
+    applyMiddleware(thunk),
 );
+
+const store = createStore(rootReducer, /* preloadedState, */ enhancer);
 
 /*store.subscribe(() => {
   console.dir(store.getState());
