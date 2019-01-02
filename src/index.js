f173aeea81348a12da7af68bac7466ed1308acd5
penji <11308346+penji@users.noreply.github.com>
2019-01-03 02:56:14 +0900

    App component 위치 이동

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import './index.css';
-import App from './component/App';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 import store from './store/index';
 
