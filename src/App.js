ca80b9ac3b5b22be9c4d5ea102fc34ae5200f44d
penji <11308346+penji@users.noreply.github.com>
2019-01-03 03:01:10 +0900

    <App> resizng 기능 분리 및 css styling 구현 변경
    - App component의 layout resizing 기능을 별도 컴포넌트 <MainFrame> 으로 분리
    - *.css 대신 styled-components 사용함

--- src/App.js
+++ src/App.js
@@ -1,77 +1,8 @@
-import lifecycle from 'page-lifecycle/dist/lifecycle.es5';
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import './App.css';
+import React from 'react';
+import MainFrame from './component/MainFrame';
 
-const BASE_SIZE = {
-  width: 320,
-  height: 480,
-  ratio: 480/320,
-};
-
-let resizeTimer;
-const resizeDelay = 100;
-
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.onResize = this.onResize.bind(this);
-    this.onResizeDebounced = this.onResizeDebounced.bind(this);
-    this.state = {
-      scale: this.onResizeDebounced(false)
-    };
-  }
-
-  componentDidMount() {
-    window.addEventListener("resize", this.onResize);
-  };
-
-  componentWillUnmount() {
-    window.removeEventListener("resize", this.onResize);
-  };
-
-  onResize() {
-    //Debouncing window.onresize
-    clearTimeout(resizeTimer);
-    resizeTimer = setTimeout(this.onResizeDebounced, resizeDelay);
-  }
-
-  onResizeDebounced(changeState = true) {
-    const {width, height, ratio} = BASE_SIZE;
-    const {innerWidth, innerHeight} = window;
-
-    const windowRatio = innerHeight / innerWidth;
-    let newScale;
-    if (windowRatio <= ratio) {
-      newScale = innerHeight / height;
-    } else {
-      newScale = innerWidth / width;
-    }
-
-    if (changeState) {
-      this.setState({
-        scale: newScale
-      });
-    } else {
-      return newScale;
-    }
-  }
-
-  render() {
-    return (
-        <div className="App" style={{
-          transform: `scale(${this.state.scale})`,
-        }}>
-
-        </div>
-    );
-  }
-}
-
-const mapStateToProps = state => state;
-
-const mapDispatchToProps = dispatch => ({});
-
-App = connect(mapStateToProps, mapDispatchToProps)(App);
+const App = () => (
+    <MainFrame/>
+);
 
 export default App;
---
+++ src/component/MainFrame.js
@@ -0,0 +1,84 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const Scalable = styled.div`
+  position: absolute;
+  background: black;
+  top:50%;
+  left:50%;
+  ${({frameWidth, frameHeight, frameScale}) => `
+    width: ${frameWidth}px;
+    height: ${frameHeight}px;
+    margin: ${- frameHeight / 2}px 0 0 ${- frameWidth / 2}px;
+    transform: scale(${frameScale});
+  `}
+`;
+
+const BASE_SIZE = {
+  width: 320,
+  height: 480,
+  ratio: 480/320,
+};
+
+let resizeTimer;
+const resizeDelay = 100;
+
+export default class MainFrame extends Component {
+  constructor(props) {
+    super(props);
+    this.onResize = this.onResize.bind(this);
+    this.onResizeDebounced = this.onResizeDebounced.bind(this);
+    this.state = {
+      scale: this.onResizeDebounced(false)
+    };
+  }
+
+  componentDidMount() {
+    window.addEventListener("resize", this.onResize);
+  };
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.onResize);
+  };
+
+  onResize() {
+    //Debouncing window.onresize
+    clearTimeout(resizeTimer);
+    resizeTimer = setTimeout(this.onResizeDebounced, resizeDelay);
+  }
+
+  onResizeDebounced(changeState = true) {
+    const {width, height, ratio} = BASE_SIZE;
+    const {innerWidth, innerHeight} = window;
+
+    const windowRatio = innerHeight / innerWidth;
+    let newScale;
+    if (windowRatio <= ratio) {
+      newScale = innerHeight / height;
+    } else {
+      newScale = innerWidth / width;
+    }
+
+    if (changeState) {
+      this.setState({
+        scale: newScale
+      });
+    } else {
+      return newScale;
+    }
+  }
+
+  render() {
+    return (
+        <Scalable
+          frameWidth={BASE_SIZE.width}
+          frameHeight={BASE_SIZE.height}
+          frameScale={this.state.scale}
+        >
+
+        </Scalable>
+    );
+  }
+}
+
+
