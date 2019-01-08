5e3794b3dbe3e5f5687670edab8492473b0c1e86
penji <11308346+penji@users.noreply.github.com>
2019-01-09 05:53:43 +0900

    <NextBlock> 1차 구현
    <InfoBoard>에서 보여주는 다음 블록을 그리는 컴포넌트.
    당장은 <Block>과 비교해 위치 조정 말고는 큰 차이가 없으나
    추후 사이즈 조정, 리팩토링 등을 고려해 우선 별도 컴포넌트로 둠.

---
+++ src/component/block/NextBlock.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import styled from 'styled-components';
+import {Tetromino, BLOCK} from '../../model/Tetromino';
+import {Cell} from './Cell';
+
+
+const Wrapper = styled.div`
+  line-height: 1px;
+  text-align: center;
+`;
+const Row = styled.div`
+  line-height: 1px;
+`;
+
+
+export const NextBlock = ({type = BLOCK.X, rotate = 0, style}) => {
+  let shape;
+  if (type === BLOCK.X) {
+    shape = [[BLOCK.X]];
+  } else {
+    shape = Tetromino[type].rotate[rotate].shape;
+  }
+
+  return (
+      <Wrapper style={style}>
+        {
+          shape.map((row, y) => (
+              <Row key={y}>
+                {
+                  row.map((cellType, x) => (
+                      <Cell key={x} type={shape[y][x]}/>
+                  ))
+                }
+              </Row>
+          ) )
+        }
+      </Wrapper>
+  );
+};
