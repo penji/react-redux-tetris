9925e31132a3f1331caf525fce3e21a3c10b163c
penji <11308346+penji@users.noreply.github.com>
2019-01-09 06:19:24 +0900

    game 상태 PAUSED/RESUMED 추가
    - 게임 시작 여부와 일시정지/재개 상태를 구분할 수 있도록 상태 재정의

--- src/action/game.js
+++ src/action/game.js
@@ -1,9 +1,13 @@
 import { createActions } from 'redux-actions';
 
 export const READY = 'READY';
+export const PAUSED = 'PAUSED';
+export const RESUMED = 'RESUMED';
 export const GAME_IS_ON = 'GAME_IS_ON';
 
 export const gameAction = createActions({
   [GAME_IS_ON]: undefined,
   [READY]: undefined,
+  [PAUSED]: undefined,
+  [RESUMED]: undefined,
 });
--- src/reducer/game.js
+++ src/reducer/game.js
@@ -2,15 +2,46 @@ import { handleActions } from 'redux-actions';
 import update from 'immutability-helper';
 import {makeCombineActionType} from '../util/util';
 
-import {gameAction} from '../action/game';
+import {
+  READY,
+  GAME_IS_ON,
+  PAUSED,
+  RESUMED,
+  gameAction,
+} from '../action/game';
+
+const mapActionToState = {
+  [READY]: {
+    playing: false,
+    paused: false,
+  },
+  [GAME_IS_ON]: {
+    playing: true,
+    paused: false,
+  },
+  [PAUSED]: {
+    playing: true,
+    paused: true,
+  },
+  [RESUMED]: {
+    playing: true,
+    paused: false,
+  }
+
+};
 
 export const game = handleActions(
     {
-      [makeCombineActionType(gameAction)]: (state, {type}) => update(state, {
-        state: {$set: type}
-      })
+      [makeCombineActionType(gameAction)]: (state, {type}) => {
+        return update(state, {$set: {
+            ...mapActionToState[type],
+            state: type,
+          }})
+      }
     },
     {
       state: 'READY',
+      playing: false,
+      paused: false,
     }
 );
