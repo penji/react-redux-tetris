774c3606916297186eb08a8fb8732dff2442c2f1
penji <11308346+penji@users.noreply.github.com>
2019-01-09 04:04:35 +0900

    기존의 <Counter>를 react-countup 라이브러리로 대체

--- src/component/infoboard/Counter.js
+++ src/component/infoboard/Counter.js
@@ -1,110 +1,44 @@
 import React, {Component} from 'react';
-import {Value} from './components';
+import CountUp from 'react-countup';
+
+import styled from 'styled-components';
+
+const StyledCountUp = styled(CountUp)`
+  display: block;
+  font-weight: 700;
+  font-family: ARCADECLASSIC;
+  text-align: right;
+  font-size: 17px;
+  bottom: 0px;
+  right: 0px;
+  line-height: 1;
+`;
 
 export default class Counter extends Component {
-  static comma(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-  }
-
-  static get fps() {
-    return 60;
-  }
-
-  static get duration() {
-    return 500;
-  }
-
-  static get period() {
-    return Math.floor(1000 / Counter.fps);
-  }
-
-  static get tick() {
-    const tick = Counter.duration / Counter.period;
-
-    // 높은 fps에서 duration을 준수하기 위한 보정
-    if (Counter.fps > 30) {
-      return Math.floor(tick * 0.9);
-    } else {
-      return Math.floor(tick);
-    }
-
-  }
-
   constructor(props) {
     super(props);
     this.state = {
-      now: 0,
       begin: 0,
     };
-    this.timerId = null;
-    this.tickCount = 0;
   }
 
   render() {
-    return <Value>{Counter.comma(this.state.now)}</Value>
+    return (
+        <StyledCountUp
+            start={this.state.begin}
+            end={this.props.value}
+            useEasing={false}
+            separator=","
+            duration={0.5}
+        />
+    );
   }
 
   componentDidUpdate(prevProps, prevState, snapshot) {
-    if (this.props.value === this.state.now) {
-      this.clearTimer();
-    } else if (this.timerId === null) {
-      this.startTimer();
-    }
-
     if (prevProps.value !== this.props.value) {
-      this.clearTimer(true);
-      this.startTimer(true);
+     this.setState({
+       begin: prevProps.value
+     })
     }
-  }
-
-  componentDidMount() {
-    this.startTimer();
-  }
-
-
-  clearTimer(valueChanged = false) {
-    if (!valueChanged) {
-      this.tickCount = 0;
-    }
-    clearInterval(this.timerId);
-    this.timerId = null;
-  }
-
-  startTimer(valueChanged = false) {
-    let next, _delta;
-
-    const {value} = this.props;
-    const {begin, now} = this.state;
-
-    if (valueChanged) {
-      _delta = Math.ceil((value - now) / (Counter.tick - this.tickCount));
-    } else {
-      _delta = Math.ceil((value - begin) / Counter.tick);
-    }
-
-    this.timerId = setInterval(() => {
-
-      const {value} = this.props;
-      const {now} = this.state;
-      this.tickCount++;
-
-      const newState = {
-        ...this.state,
-      };
-
-      if (this.tickCount === Counter.tick) {
-        next = value;
-        newState.begin = value;
-      } else {
-        next = now + _delta;
-      }
-
-      newState.now = next;
-
-
-
-      this.setState(newState);
-
-    }, Counter.period);
   }
 }
