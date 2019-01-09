ca2d00d04a976b8fe5618d481e48b28a406b68a7
penji <11308346+penji@users.noreply.github.com>
2019-01-09 23:07:54 +0900

    사용하지 않는 폰트 리소스 및 import 삭제

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -1,11 +1,7 @@
 import { createGlobalStyle } from 'styled-components';
-import ARCADECLASSIC from './resource/ARCADECLASSIC.TTF';
 
 const GlobalStyle = createGlobalStyle`
-  @font-face {
-    font-family: ARCADECLASSIC;
-    src: url(${ARCADECLASSIC});
-  }
+  
 `;
 
 export default GlobalStyle;
