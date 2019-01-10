2972c251edfe4c6c0da158999ecee43df840d398
penji <11308346+penji@users.noreply.github.com>
2019-01-10 22:54:27 +0900

    Updates

---
+++ precache-manifest.fb5a7e7d17e836438de7cff5c25362dd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "52becac2a528fe0d27fa",
+    "url": "/react-redux-tetris/static/js/runtime~main.52becac2.js"
+  },
+  {
+    "revision": "37e99ee28374b43ec5eb",
+    "url": "/react-redux-tetris/static/js/main.37e99ee2.chunk.js"
+  },
+  {
+    "revision": "9640bf2584cb65833155",
+    "url": "/react-redux-tetris/static/js/1.9640bf25.chunk.js"
+  },
+  {
+    "revision": "37e99ee28374b43ec5eb",
+    "url": "/react-redux-tetris/static/css/main.7addff92.chunk.css"
+  },
+  {
+    "revision": "e44a83599c53dbe9897d672e79ac9614",
+    "url": "/react-redux-tetris/index.html"
+  }
+];
