f0a8644bfcbf13fecdf64342affb8e813b7ae194
penji <11308346+penji@users.noreply.github.com>
2020-01-12 00:23:40 +0900

    Updates

---
+++ precache-manifest.eb81b0a19cbd333382a2cce7ccaf4f2d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "daf163bf9e83934d7dde837de16c1829",
+    "url": "/react-redux-tetris/index.html"
+  },
+  {
+    "revision": "b931f20635a753955ce9",
+    "url": "/react-redux-tetris/static/css/main.462b0bd9.chunk.css"
+  },
+  {
+    "revision": "1c7b9e6a8089a17ce903",
+    "url": "/react-redux-tetris/static/js/2.e0df1bda.chunk.js"
+  },
+  {
+    "revision": "26286e24aec774779ccbd06989683075",
+    "url": "/react-redux-tetris/static/js/2.e0df1bda.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b931f20635a753955ce9",
+    "url": "/react-redux-tetris/static/js/main.77b4d605.chunk.js"
+  },
+  {
+    "revision": "248f81b0b7f51c550fb1",
+    "url": "/react-redux-tetris/static/js/runtime-main.2504aae1.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-redux-tetris/precache-manifest.94a13528dcc62b81aa5d0b3ba2ee8a00.js"
+  "/react-redux-tetris/precache-manifest.eb81b0a19cbd333382a2cce7ccaf4f2d.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-redux-tetris/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-redux-tetris/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
